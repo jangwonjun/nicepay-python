87c1c347a814ef8a052cb6b466d1656a5612c090
wonjunJang <nuriyooyoung@gmail.com>
2023-11-09 14:39:03 +0900

    modify

--- main.py
+++ main.py
@@ -68,6 +68,5 @@ def discard_order():
     return render_template('index.html', discard_message=discard_message)
 
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=FLASK_ENUM.PORT)
