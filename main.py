d94446aab71660844185814edb9d9c04c450c6db
wonjunJang <nuriyooyoung@gmail.com>
2023-11-09 14:30:38 +0900

    bag&disgard

--- main.py
+++ main.py
@@ -65,6 +65,5 @@ def discard_order():
     return render_template('index.html', discard_message=discard_message)
 
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=FLASK_ENUM.PORT)
