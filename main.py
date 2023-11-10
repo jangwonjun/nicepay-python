0f621d4dc838804d66e29977361962fc45b05e09
wonjunJang <nuriyooyoung@gmail.com>
2023-11-10 13:54:41 +0900

    modify

--- main.py
+++ main.py
@@ -79,36 +79,10 @@ def coupon_making():
         coupon_result.append(str(random.randint(0, 9)))
     
     certification_code = ''.join(coupon_result)
-    certification_code = certification_code + 'ddbk'
+    session['certification_code'] = certification_code
         
     return certification_code
 
-@app.route('/information',methods=['GET','POST'])
-def information():
-    #사용자의 결제 정보를 위한 정보를 입력받습니다. 
-    #이메일과 카카오톡 알람톡을 전송할 전화번호를 수집합니다. 
-    #이메일은 unique 값으로 처리를 하였기 때문에, 중복을 예방합니다. 
-    
-    conn, cursor = get_db_direct()
-    email = request.form.get('email')
-    phone_number = request.form.get('tel')
-    print(email,phone_number)
-    session['email'] = email
-    session['phone_number'] = phone_number
-    
-    try : 
-        cursor.execute(f"USE {SQL.DB_NAME}")
-        sql_query = "INSERT INTO login (name, phone_number) VALUES (%s, %s)"
-        cursor.execute(sql_query, (email,phone_number))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        print("사용자의 정보가 입력되었습니다.")
-    except pymysql.Error as e :
-        print(f"SQL 구문 오류가 발생하였습니다. {e}")
-    
-    return render_template('login.html')
-'''
 @app.route('/registration_auto',methods=['POST','GET'])
 def registration_auto():
    #결제 완료전의 주문한 쿠폰을 서버에 저장합니다. 
@@ -116,21 +90,50 @@ def registration_auto():
    conn, cursor = get_db_direct()
    email = session.get('email')
    phone_number = session.get('phone_number')
+   certification_code = session.get('certification_code')
    
    try : 
         cursor.execute(f"USE {SQL.DB_NAME}")
         sql_query = "INSERT INTO pay (pay_num, pay_state) VALUES (%s, %s)"
-        cursor.execute(sql_query, (email,'0'))
+        cursor.execute(sql_query, (certification_code,'0'))
         conn.commit()
         cursor.close()
         conn.close()
-        print("사용자의 정보가 입력되었습니다.")
+        print("쿠폰 정보가 입력되었습니다.")
         
-    except pymysql.Error as e :
+   except pymysql.Error as e :
         print(f"SQL 구문 오류가 발생하였습니다. {e}")
     
+   return render_template('login.html')
+
+
+@app.route('/information',methods=['GET','POST'])
+def information():
+    #사용자의 결제 정보를 위한 정보를 입력받습니다. 
+    #이메일과 카카오톡 알람톡을 전송할 전화번호를 수집합니다. 
+    #이메일은 unique 값으로 처리를 하였기 때문에, 중복을 예방합니다. 
+    conn, cursor = get_db_direct()
+    certification_code = session.get('certification_code')
+    email = request.form.get('email')
+    phone_number = request.form.get('tel')
+    print(email,phone_number)
+
+    try : 
+        #SQL 5칸
+        cursor.execute(f"USE {SQL.DB_NAME}")
+        sql_query = "INSERT INTO user (email, phone_number,coupon_number,coupon_state, pay_state,online_pay) VALUES (%s, %s, %s, %s, %s , %s)"
+        cursor.execute(sql_query, (email,phone_number,int(certification_code),'0','0','0'))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("사용자와 쿠폰 번호 정보가 입력되었습니다.")
+    except pymysql.Error as e :
+        print(f"SQL 구문 오류가 발생하였습니다. {e}")
+         
     return render_template('login.html')
-'''
+
+    
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=FLASK_ENUM.PORT)
